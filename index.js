7d3270bc25207fbc954d6b240da23f7105736906
LocalUser <121312523+localuser-isback@users.noreply.github.com>
2023-06-21 11:49:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const fs = require('fs');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const express = require('express');
-const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
 
 // Check if the current process is the master process
 if (cluster.isMaster) {
-  // Fork worker processes based on the number of CPUs
+  // Track the current load on the first core and the second core worker
+  let firstCoreLoad = 0;
+  let secondCoreWorker = null;
+
+  // Fork worker processes based on the number of CPUs except the second one
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    if (i !== 1) {
+      cluster.fork();
+    }
   }
 
   cluster.on('exit', (worker, code, signal) => {
@@ -16,33 +22,48 @@ if (cluster.isMaster) {
     // Fork a new worker if an existing worker dies
     cluster.fork();
   });
+
+  // Update the first core load and manage second core worker
+  setInterval(() => {
+    const workers = Object.values(cluster.workers);
+    const firstCoreWorker = workers[0];
+    if (firstCoreWorker && firstCoreWorker.process) {
+      const cpuUsage = firstCoreWorker.process.cpuUsage();
+      const totalUsage = cpuUsage.user + cpuUsage.system;
+      const loadPercentage = (totalUsage / 1000000) * 100; // Convert to percentage
+      firstCoreLoad = loadPercentage;
+
+      // Check if the second core worker needs to be stopped or started
+      if (firstCoreLoad >= 90 && !secondCoreWorker) {
+        secondCoreWorker = cluster.fork();
+        console.log('Second core worker started');
+      } else if (firstCoreLoad < 90 && secondCoreWorker) {
+        secondCoreWorker.kill();
+        secondCoreWorker = null;
+        console.log('Second core worker stopped');
+      }
+    }
+  }, 1000);
 } else {
   const app = express();
 
   // Serve static files from the "public" directory
   app.use(express.static('site'));
 
-  // Middleware to inject style.css into the head of HTML files
+  // Inject the style.css file into the head of HTML files
   app.use((req, res, next) => {
-    const filePath = __dirname + '/public' + req.url;
-    if (filePath.endsWith('.html')) {
-      fs.readFile(filePath, 'utf8', (err, html) => {
+    if (req.url.endsWith('.html')) {
+      const filePath = path.join(__dirname, 'site', req.url);
+      fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
-          console.error(`Error reading file: ${filePath}`);
-          return res.status(500).send('Internal Server Error');
+          return next();
         }
 
-        const $ = cheerio.load(html);
-        const head = $('head');
-        const styleTag = `<link rel="stylesheet" type="text/css" href="/style.css">`;
-
-        // Append the style.css link tag to the head of the HTML file
-        head.append(styleTag);
-
-        // Get the modified HTML content
-        const modifiedHtml = $.html();
-
-        res.send(modifiedHtml);
+        const modifiedData = data.replace(
+          '<head>',
+          `<head><link rel="stylesheet" href="style.css">`
+        );
+        res.send(modifiedData);
       });
     } else {
       next();
