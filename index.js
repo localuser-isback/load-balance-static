a60d2cc3bc6b6cc003ce34e2ca91471e77292ddf
LocalUser <121312523+localuser-isback@users.noreply.github.com>
2023-06-21 11:25:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const express = require('express');
+const cheerio = require('cheerio');
 
 // Check if the current process is the master process
 if (cluster.isMaster) {
@@ -18,7 +20,34 @@ if (cluster.isMaster) {
   const app = express();
 
   // Serve static files from the "public" directory
-  app.use(express.static('public'));
+  app.use(express.static('site'));
+
+  // Middleware to inject style.css into the head of HTML files
+  app.use((req, res, next) => {
+    const filePath = __dirname + '/public' + req.url;
+    if (filePath.endsWith('.html')) {
+      fs.readFile(filePath, 'utf8', (err, html) => {
+        if (err) {
+          console.error(`Error reading file: ${filePath}`);
+          return res.status(500).send('Internal Server Error');
+        }
+
+        const $ = cheerio.load(html);
+        const head = $('head');
+        const styleTag = `<link rel="stylesheet" type="text/css" href="/style.css">`;
+
+        // Append the style.css link tag to the head of the HTML file
+        head.append(styleTag);
+
+        // Get the modified HTML content
+        const modifiedHtml = $.html();
+
+        res.send(modifiedHtml);
+      });
+    } else {
+      next();
+    }
+  });
 
   // Start the server
   const port = 3000;
